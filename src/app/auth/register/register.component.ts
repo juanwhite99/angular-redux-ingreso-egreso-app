e30a5b4a87bd75514a06ad0dc7fc73f7213f5db2
JP <juan.pablo.lb99@gmail.com>
2021-10-24 15:07:51 -0500

    End of chapter 72

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +7,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  formRegister!: FormGroup;
 
-  constructor() { }
+  get validName() {
+    return this.formRegister.controls.nombre.valid;
+  }
+
+  get validEmail() {
+    return this.formRegister.controls.correo.valid;
+  }
+
+  get validPassword() {
+    return this.formRegister.controls.password.valid;
+  }
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.formRegister = this.fb.group({
+      nombre: ['', Validators.required],
+      correo: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+    });
+  }
+
+  register() {
+    console.log(this.formRegister);
+    console.log(this.formRegister.valid);
+    console.log(this.formRegister.value);
   }
 
 }
